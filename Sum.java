9d675ebb98792c342fcdcab9ab939592ebd65eed
vanshsaini1374 <vsaini875560@gmail.com>
2026-09-14 22:55:38 +0530

    Sum : Add Two Numbers

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String[] args) {
+        int firstNumber = 998;
+        int secondNumber = 112;
+        int sum = firstNumber + secondNumber;
+        System.out.println("The sum is: " + sum);
+    }
+}
